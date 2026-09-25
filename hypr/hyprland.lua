666f8440a007b52f6616acbff34a66cfcda0661c
Felix Eidler <felixeidler@gmail.com>
2026-09-25 10:31:44 +0200

    ch ch ch changes

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,9 +1,14 @@
 hl.config({
     input = {
         kb_layout = "de",
-	
-	follow_mouse = 1,
-    },
+
+		follow_mouse = 1,
+	},
+})
+
+hl.device({
+	name = "elan050b:00-04f3:31c3-touchpad",
+	enabled = true
 })
 
 hl.bind("SUPER + Q", hl.dsp.exec_cmd("kitty"))
@@ -16,8 +21,3 @@ hl.bind("SUPER + left", hl.dsp.focus({ direction = "left" }))
 hl.bind("SUPER + right", hl.dsp.focus({ direction = "right" }))
 hl.bind("SUPER + up", hl.dsp.focus({ direction = "up" }))
 hl.bind("SUPER + down", hl.dsp.focus({ direction = "down" }))
-
-hl.device({
-	name = "elan050b:00-04f3:31c3-mouse",
-	enabled = true,
-})
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,31 @@
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
+	Plug('folke/tokyonight.nvim')
 
 vim.call('plug#end')
 
 vim.lsp.config['lua-server'] = {
 
     cmd = { 'lua-language-server' },
-
-
     filetypes = { 'lua' },
+	root_dir = vim.fs.dirname(vim.fs.find({'.git', '.vim', 'nvim'}, { upward = true })[1]),
+settings = { Lua = { diagnostics = { globals = {'vim'} } } },
 }
 
 vim.lsp.enable('lua-server')
-vim.lsp.inline_completion.enable()
+vim.keymap.set('i', '<Tab>', function()
 
--- vim.keymap.set('SUPER', function() )
+	if not vim.lsp.inline_completion.get()
+		then
+			return '<Tab>'
+	end
+end, { expr = true, desc = 'Accept the current inline completion' })
+
+vim.lsp.inline_completion.enable()
+vim.lsp.codelens.enable()
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+
+vim.cmd.colorscheme "tokyonight-night"
